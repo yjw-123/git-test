0aff484f4439dbe8289cfa59cf131713215bd12e
junwu <2535641815@qq.com>
2022-03-27 12:44:22 +0800

    push commit

--- src/main/java/com/junwu/git/GitTest.java
+++ src/main/java/com/junwu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello world4!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
+
     }
 }
